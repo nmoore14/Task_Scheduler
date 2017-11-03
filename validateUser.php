1b7a61ebc0cef2cf5d7603f3c9421311b6f1bfd2
Nick Moore <nickmoore_1@live.com>
2017-11-02 20:20:04 -0400

    This is the validateUser.php file

---
+++ validateUser.php
@@ -0,0 +1,44 @@
+<?php
+
+    function isValidUser($userName, $password) {
+        $hash = getPasswordHash($userName);
+        $isValid = password_verify($password, $hash);
+        return isValid;
+    }
+
+    function getPasswordHash($userName) {
+        $user = 'username';
+        $pass = 'password';
+        $dbname = 'database';
+        try {
+            $pdo = new PDO('mysql:host=localhost;dbname=' . $dbname, $user, $pass);
+            $hash = "";
+            $resultSet = $pdo->query("SELECT Password FROM tasks_user WHERE UserName = '$userName'");
+
+            foreach ($resultSet as $row) {
+                $hash = $row['Password'];
+            }
+        } catch(PDOException $err) {
+            echo $err->getMessage();
+        }
+        return $hash;
+    }
+
+    function getUsersFirstName($userName) {
+        $user = 'username';
+        $pass = 'password';
+        $dbname = 'database';
+        try {
+            $pdo = new PDO('mysql:host=localhost;dbname=' . $dbname, $user, $pass);
+            $firstName = "";
+            $resultSet = $pdo->query("SELECT FirstName FROM tasks_user WHERE UserName = '$userName'");
+
+            foreach ($resultSet as $row) {
+                $firstName = $row['FirstName'];
+            }
+        } catch(PDOException $err) {
+            echo $err->getMessage();
+        }
+        return $firstName;
+    }
+?>
