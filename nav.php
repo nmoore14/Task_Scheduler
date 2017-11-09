75794e0b7ed78489a45ccee7a82392fc56807784
Nicholas Moore <nmoore2@gtest.northeaststate.edu>
2017-11-09 16:39:08 -0500

    Created a logout file and a link in the nav file

--- nav.php
+++ nav.php
@@ -5,18 +5,13 @@
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
             </button>
-            <!-- Commented out until further development
             <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
                 <ul class="navbar-nav">
-                    <li class="nav-item"  id="homeLink">
-                        <a class="nav-link disabled" href="#">Edit</a>
-                    </li>
-                    <li class="nav-item" id="aboutLink">
-                        <a class="nav-link disabled" href="#">Settings</a>
+                    <li class="nav-item"  id="logoutLink">
+                        <a class="nav-link" href="logout.php"><i class="fa fa-sign-out"></i> Log Out</a>
                     </li>
                 </ul>
             </div>
-            -->
         </nav>
     </div>
 </div>
